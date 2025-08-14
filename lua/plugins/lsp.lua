a730c6119c4b95af5b08cf39b2169ff6cff7477c
tima gutmensch <tima_gutmensch+github@proton.me>
2025-08-14 09:28:07 +0800

    refactor: separate filetype plugins with lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,4 +7,21 @@ return {
       },
     },
   },
+  {
+    "nvim-flutter/flutter-tools.nvim",
+    ft = { "dart" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "fzhnf/flutter-bloc.nvim",
+    lazy = true,
+    ft = { "dart" },
+    opts = {
+      bloc_type = "default", -- Choose from: 'default', 'equatable', 'freezed'
+      use_sealed_classes = false,
+      enable_code_actions = true,
+    },
+  },
 }
